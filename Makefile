b2b0046729dd71c5d12842de07138a6cba8fae49
Jeremy Olexa <jolexa@pagerduty.com>
2017-09-15 16:06:19 -0500

    Initial work/deploy

---
+++ Makefile
@@ -0,0 +1,24 @@
+STACKNAME_BASE=pd-oncall-chat-topic
+REGION="ca-central-1"
+SSMKeyArn=$(shell aws kms --region $(REGION) describe-key --key-id alias/aws/ssm --query KeyMetadata.Arn)
+BUCKET=$(STACKNAME_BASE)
+MD5=$(shell md5sum lambda/*.py| md5sum | cut -d ' ' -f 1)
+
+
+deploy:
+	cd lambda && \
+		zip -r9 /tmp/deployment.zip *.py && \
+		aws s3 cp --region $(REGION) /tmp/deployment.zip \
+			s3://$(BUCKET)/$(MD5) && \
+		rm -f /tmp/deployment.zip
+	aws cloudformation deploy \
+		--template-file deployment.yml \
+		--stack-name $(STACKNAME_BASE) \
+		--region $(REGION) \
+		--parameter-overrides \
+		"Bucket=$(BUCKET)" \
+		"md5=$(MD5)" \
+		"SSMKeyArn"=$(SSMKeyArn) \
+		"SSMKeyName"=$(STACKNAME_BASE) \
+		--capabilities CAPABILITY_IAM || exit 0
+
---
+++ lambda/main.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import os
+import datetime
+from botocore.vendored import requests
+import boto3
+
+API_KEY = boto3.client('ssm').get_parameters(
+        Names=[os.environ['API_KEY_NAME']],
+        WithDecryption=True)['Parameters'][0]['Value']
+
+headers = {
+        'Accept': 'application/vnd.pagerduty+json;version=2',
+        'Authorization': 'Token token={token}'.format(token=API_KEY)
+        }
+
+# Get the Current User on-call for a given schedule
+def get_user(schedule_id):
+    global headers
+    url = 'https://api.pagerduty.com/schedules/{0}/users'.format(
+        schedule_id
+    )
+    # This value should be less than the running interval
+    t = datetime.datetime.now() - datetime.timedelta(minutes=1)
+    payload = {}
+    payload['since'] = t.isoformat()
+    payload['until'] = datetime.datetime.now().isoformat()
+    r = requests.get(url, headers=headers, params=payload)
+    return = r.json()['users'][0]['name']
+
+def handler(event, context):
+    print(event)
+    print(context)
+    return get_user('P31BKVS')
+
+if __name__ == '__main__':
+    get_user('P31BKVS')
