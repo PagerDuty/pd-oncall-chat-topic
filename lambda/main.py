2151751a778edb5b5f64c28f3a27f7b649c9b018
Jeremy Olexa <jolexa@pagerduty.com>
2017-09-21 10:29:57 -0500

    Switch to logging module

--- lambda/main.py
+++ lambda/main.py
@@ -3,6 +3,7 @@
 import os
 from datetime import datetime, timezone, timedelta
 import threading
+import logging
 
 from botocore.vendored import requests
 import boto3
@@ -10,6 +11,11 @@ import boto3
 # semaphore limit of 5
 maxthreads = 5
 sema = threading.Semaphore(value=maxthreads)
+
+logging.getLogger('boto3').setLevel(logging.CRITICAL)
+logging.getLogger('botocore').setLevel(logging.CRITICAL)
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 # Fetch the PD API token from PD_API_KEY_NAME key in SSM
 PD_API_KEY = boto3.client('ssm').get_parameters(
@@ -38,10 +44,11 @@ def get_user(schedule_id):
     override = requests.get(override_schedule_url, headers=headers, params=payload)
     try:
         username = override.json()['overrides'][0]['user']['summary'] + " (Override)"
+        logger.debug("Currently on call (Override): {}".format(username))
     except IndexError:
         normal = requests.get(normal_schedule_url, headers=headers, params=payload)
         username = normal.json()['users'][0]['name']
-    print("Currently on call: {}".format(username))
+        logger.debug("Currently on call: {}".format(username))
     return username
 
 def get_pd_schedule_name(schedule_id):
@@ -57,8 +64,8 @@ def get_pd_schedule_name(schedule_id):
     try:
         return r.json()['schedule']['name']
     except KeyError:
-        print(r.status_code)
-        print(r.json())
+        logger.debug(r.status_code)
+        logger.debug(r.json())
         return None
 
 def get_slack_topic(channel):
@@ -68,10 +75,14 @@ def get_slack_topic(channel):
         WithDecryption=True)['Parameters'][0]['Value']
     payload['channel'] = channel
     r = requests.post('https://slack.com/api/channels.info', data=payload)
-    print(r.json())
+    logger.debug(r.json())
     return r.json()['channel']['topic']['value']
 
 def update_slack_topic(channel, proposed_update):
+    logger.debug("Entered update_slack_topic() with: {} {}".format(
+        channel,
+        proposed_update)
+        )
     payload = {}
     payload['token'] = boto3.client('ssm').get_parameters(
         Names=[os.environ['SLACK_API_KEY_NAME']],
@@ -92,13 +103,16 @@ def update_slack_topic(channel, proposed_update):
         r = requests.post('https://slack.com/api/channels.setTopic', data=payload)
         return r.json()
     else:
-        print("Not updating slack, topic is the same")
+        logger.debug("Not updating slack, topic is the same")
         return None
 
 def do_work(obj):
     sema.acquire()
     # entrypoint of the thread
-    print("Operating on {}".format(obj))
+    logger.debug("Operating on {}".format(obj))
+    logger.debug(type(obj))
+    logger.debug(obj)
+    logger.debug(obj.keys())
     # schedule will ALWAYS be there, it is a ddb primarykey
     schedule = obj['schedule']['S']
     username = get_user(schedule)
@@ -108,7 +122,7 @@ def do_work(obj):
         update_slack_topic(slack, topic)
     elif 'hipchat' in obj.keys():
         hipchat = i['hipchat']['S']
-        print("HipChat is not supported yet. Ignoring this entry...")
+        logger.debug("HipChat is not supported yet. Ignoring this entry...")
     sema.release()
 
 def handler(event, context):
