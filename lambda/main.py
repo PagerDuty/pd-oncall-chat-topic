45a4f751cd3aad9e19f408df76e9b4d6e3b20e88
Jeremy Olexa <jolexa@pagerduty.com>
2017-10-12 15:42:22 -0500

    Add some readability, at expense of longer line lengths

--- lambda/main.py
+++ lambda/main.py
@@ -20,26 +20,22 @@ logger.setLevel(logging.DEBUG)
 
 # Fetch the PD API token from PD_API_KEY_NAME key in SSM
 PD_API_KEY = boto3.client('ssm').get_parameters(
-        Names=[os.environ['PD_API_KEY_NAME']],
-        WithDecryption=True)['Parameters'][0]['Value']
+    Names=[os.environ['PD_API_KEY_NAME']],
+    WithDecryption=True)['Parameters'][0]['Value']
 
 
 # Get the Current User on-call for a given schedule
 def get_user(schedule_id):
     global PD_API_KEY
     headers = {
-            'Accept': 'application/vnd.pagerduty+json;version=2',
-            'Authorization': 'Token token={token}'.format(token=PD_API_KEY)
-            }
-    normal_schedule_url = (
-        'https://api.pagerduty.com/schedules/{0}/users'.format(
-            schedule_id
-        )
+        'Accept': 'application/vnd.pagerduty+json;version=2',
+        'Authorization': 'Token token={token}'.format(token=PD_API_KEY)
+    }
+    normal_schedule_url = 'https://api.pagerduty.com/schedules/{0}/users'.format(
+        schedule_id
     )
-    override_schedule_url = (
-        'https://api.pagerduty.com/schedules/{0}/overrides'.format(
-            schedule_id
-        )
+    override_schedule_url = 'https://api.pagerduty.com/schedules/{0}/overrides'.format(
+        schedule_id
     )
     # This value should be less than the running interval
     # It is best to use UTC for the datetime object
@@ -50,22 +46,18 @@ def get_user(schedule_id):
     payload['until'] = now.isoformat()
     # If there is no override, then check the schedule directly
     override = requests.get(
-            override_schedule_url,
-            headers=headers,
-            params=payload
-        )
+        override_schedule_url,
+        headers=headers,
+        params=payload
+    )
     if override.status_code == 404:
         logger.critical("ABORT: Not a valid schedule: {}".format(schedule_id))
         return False
     if override.json().get('overrides'):  # is not []
         # TODO: This doesn't work with multiple overrides for the same minute
-        username = (
-            override.json()['overrides'][0]['user']['summary'] + " (Override)"
-        )
+        username = override.json()['overrides'][0]['user']['summary'] + " (Override)"
     else:
-        normal = (
-            requests.get(normal_schedule_url, headers=headers, params=payload)
-        )
+        normal = requests.get(normal_schedule_url, headers=headers, params=payload)
         try:
             username = normal.json()['users'][0]['name']
         except IndexError:
@@ -77,12 +69,10 @@ def get_user(schedule_id):
 def get_pd_schedule_name(schedule_id):
     global PD_API_KEY
     headers = {
-            'Accept': 'application/vnd.pagerduty+json;version=2',
-            'Authorization': 'Token token={token}'.format(token=PD_API_KEY)
-            }
-    url = 'https://api.pagerduty.com/schedules/{0}'.format(
-        schedule_id
-    )
+        'Accept': 'application/vnd.pagerduty+json;version=2',
+        'Authorization': 'Token token={token}'.format(token=PD_API_KEY)
+    }
+    url = 'https://api.pagerduty.com/schedules/{0}'.format(schedule_id)
     r = requests.get(url, headers=headers)
     try:
         return r.json()['schedule']['name']
@@ -111,7 +101,7 @@ def update_slack_topic(channel, proposed_update):
     logger.debug("Entered update_slack_topic() with: {} {}".format(
         channel,
         proposed_update)
-        )
+    )
     payload = {}
     payload['token'] = boto3.client('ssm').get_parameters(
         Names=[os.environ['SLACK_API_KEY_NAME']],
@@ -131,10 +121,8 @@ def update_slack_topic(channel, proposed_update):
 
     if current_full_topic is not None:
         try:
-            first_part = current_full_topic.rsplit(
-                    '|', c_delimit_count)[0].strip()
-            second_part = current_full_topic.replace(
-                    first_part + " |", "").strip()
+            first_part = current_full_topic.rsplit('|', c_delimit_count)[0].strip()
+            second_part = current_full_topic.replace(first_part + " |", "").strip()
         except IndexError:  # if there is no '|' in the topic
             first_part = "none"
             second_part = current_full_topic
@@ -148,10 +136,7 @@ def update_slack_topic(channel, proposed_update):
         if len(topic) > 251:
             topic = topic[0:247] + "..."
         payload['topic'] = topic
-        r = requests.post(
-                'https://slack.com/api/channels.setTopic',
-                data=payload
-            )
+        r = requests.post('https://slack.com/api/channels.setTopic', data=payload)
         return r.json()
     else:
         logger.info("Not updating slack, topic is the same")
@@ -165,9 +150,9 @@ def figure_out_schedule(s):
         return s
     global PD_API_KEY
     headers = {
-            'Accept': 'application/vnd.pagerduty+json;version=2',
-            'Authorization': 'Token token={token}'.format(token=PD_API_KEY)
-            }
+        'Accept': 'application/vnd.pagerduty+json;version=2',
+        'Authorization': 'Token token={token}'.format(token=PD_API_KEY)
+    }
     url = 'https://api.pagerduty.com/schedules/'
     payload = {}
     payload['query'] = s
@@ -191,32 +176,26 @@ def do_work(obj):
     if schedule:
         username = get_user(schedule)
     else:
-        logger.critical(
-            "Exiting: Schedule not found or not valid, see previous errors"
-        )
+        logger.critical("Exiting: Schedule not found or not valid, see previous errors")
         return 127
     if username is not None:  # then it is valid and update the chat topic
         topic = "{} is on-call for '{}'".format(
-                username,
-                get_pd_schedule_name(schedule)
-            )
+            username,
+            get_pd_schedule_name(schedule)
+        )
         if 'slack' in obj.keys():
             slack = obj['slack']['S']
             update_slack_topic(slack, topic)
         elif 'hipchat' in obj.keys():
             # hipchat = obj['hipchat']['S']
-            logger.critical(
-                "HipChat is not supported yet. Ignoring this entry..."
-            )
+            logger.critical("HipChat is not supported yet. Ignoring this entry...")
     sema.release()
 
 
 def handler(event, context):
     print(event)
     ddb = boto3.client('dynamodb')
-    response = ddb.scan(
-                TableName=os.environ['CONFIG_TABLE'],
-                )
+    response = ddb.scan(TableName=os.environ['CONFIG_TABLE'])
     threads = []
     for i in response['Items']:
         thread = threading.Thread(target=do_work, args=(i,))
