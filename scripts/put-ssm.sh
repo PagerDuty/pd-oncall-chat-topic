49ac4ccbf367ba2b3d50066685b852edea73a55b
Max Englander <max.englander@teacherspayteachers.com>
2020-11-24 19:23:01 -0500

    scripts: allow overwrite ssm parameter

--- scripts/put-ssm.sh
+++ scripts/put-ssm.sh
@@ -10,4 +10,5 @@ read -sp "API Key (not eched to terminal): " passwd; echo
 aws ssm --region ${3} \
     put-parameter --name ${1} \
     --description "Used by stack: ${2}" \
+    --overwrite \
     --value ${passwd} --type SecureString
